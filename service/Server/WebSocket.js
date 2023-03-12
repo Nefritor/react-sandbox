37cd373448fd57d7002b63aa7bcf1e87d87d200e
NEFRITOR-PC\Nefritor <nefritor@gmail.com>
2023-03-12 23:14:30 +0500

    v0.4 - Messenger v2 Release

--- service/Server/WebSocket.js
+++
@@ -1,8 +0,0 @@
-export const getBroadcaster = (wss) =>
-    (data, selector) => {
-        wss.clients.forEach((client) => {
-            if (!selector || selector(client)) {
-                client.send(JSON.stringify(data));
-            }
-        })
-    }
