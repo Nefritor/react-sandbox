595440992ad0e52c97c75a8bb153024d2b78fa19
NEFRITOR-PC\Nefritor <nefritor@gmail.com>
2023-05-16 22:37:57 +0500

    v0.6.1 - added exercise create, read, delete

--- src/Constructor/_rpc/Base.ts
+++ src/Constructor/_rpc/Base.ts
@@ -2,6 +2,6 @@ import {createController, getController} from 'Utils/RPC';
 
 const RPC_ID: string = 'exerciseConstructor';
 
-createController(RPC_ID, {secured: 5556, unsecured: 5555});
+createController(RPC_ID, {secured: 1235, unsecured: 1234});
 
 export const Controller = getController(RPC_ID);
